2078ed7bded0d0f01cc5123863b8d3bdef3c5269
vinay-kumar-ps <vinaykumarps321@gmail.com>
2023-12-20 23:11:56 +0530

    added user struct

--- pkg/domain/user.go
+++ pkg/domain/user.go
@@ -1,9 +1,15 @@
- package domain
+package domain
 
- type User struct{
-	
- 
+//user represents a user in the system
+
+type User struct {
+	ID int `gorm:"primarykey"`
+	Name string `json:"name"`
+	Email string `gorm:"unique" json:"email"`
+	Username string `json:"username"`
+    Password string `json:"password"`
+	Phone string `gorm:"unique" json:"phone"`
+	Permission bool `gorm:"default:true" json:"permission"`
 }
-type Address struct{
-	
+type Address struct {
 }

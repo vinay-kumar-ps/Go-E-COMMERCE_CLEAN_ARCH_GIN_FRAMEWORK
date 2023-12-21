1a87fab1eb6d91f7412c61dbe889ddd48050e75f
vinay-kumar-ps <vinaykumarps321@gmail.com>
2023-12-21 15:08:09 +0530

    admin functionality

--- pkg/utils/models/admin.go
+++ pkg/utils/models/admin.go
@@ -1,1 +1,32 @@
 package models
+
+import "time"
+
+type AdminLogin struct {
+	Email    string `json:"email,omitempty" validate:"required"`
+	Password string `json:"password" validate:"min=8,max20"`
+}
+type AdminDetailsResponse struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type TokenAdmin struct {
+	Username string
+	Token    string
+}
+type UserDetailsAtAdmin struct {
+	Id         int    `json:"id"`
+	Name       string `json:"name"`
+	Email      string `json:"email"`
+	Phone      string `json:"phone"`
+	Permission bool   `json:"permission"`
+}
+//type AdminSaleReport struct{
+// orders models.order
+//}
+ type CustomDates struct{
+	StartingDate time.Time `json:"startingDate"`
+	EndDate time.Time`json:"endDate"`
+ }

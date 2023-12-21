0d713882ac9ea24a8ad7e2c1011a2ad0fcf67227
vinay-kumar-ps <vinaykumarps321@gmail.com>
2023-12-21 23:31:31 +0530

    added user cart

--- pkg/utils/models/cart.go
+++ pkg/utils/models/cart.go
@@ -1,1 +1,22 @@
 package models
+
+import "ecommerce/pkg/domain"
+
+type Getcart struct {
+	ProductName string `json:"product_name"`
+	Category_id int `json:"category_id"`
+	Quantity int `json:"quantity"`
+	Total float64      `json:"total_price"`
+	 DiscountedPrice float64 `json:"discounted_price"`
+}
+type CheckOut struct{
+	Adressess []domain.Address
+	Products []Getcart
+	PaymentMethod[]domain.PaymentMethod
+	TotalPrice float64
+}
+type Order struct{
+	AdressId int `json:"address_id"`
+	PaymentID int `json:"payment_ID"`
+
+}

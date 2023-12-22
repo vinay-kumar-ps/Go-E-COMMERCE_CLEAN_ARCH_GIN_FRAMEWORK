3601b4454ad2729ca2201df3767dadcc6b8384d6
vinay-kumar-ps <vinaykumarps321@gmail.com>
2023-12-22 12:22:24 +0530

    added user inventory details

---
+++ pkg/utils/models/inventory.go
@@ -0,0 +1,40 @@
+package models
+
+type InventoryResponse struct {
+	productID int
+	//stock int
+}
+
+type Inventory struct {
+	ID          uint    `json:"id"`
+	CategoryID  int     `json:"category_id"`
+	Image       string  `json:"image"`
+	ProductName string  `json:"productname"`
+	Description string  `json:"description"`
+	Stock       int     `json:"stock"`
+	Price       float64 `json:"price"`
+}
+type UpadateInventory struct {
+	CategoryID  int     `json:"category_id"`
+	ProductName string  `json:"productName"`
+	Description string  `json:"description"`
+	Stock       int     `json:"stock"`
+	Price       float64 `json:"price"`
+}
+type InventoryList struct {
+	ID          uint    `json:"id"`
+	Category    string  `json:"category"`
+	Image       string  `json:"image"`
+	ProductName string  `json:"productName"`
+	Description string  `json:"description"`
+	Stock       int     `json:"stock"`
+	Price       float64 `json:"price"`
+}
+type InventoryDetails struct{
+	Inventory Inventory
+	AdditionalImages []ImagesInfo
+}
+type ImagesInfo struct{
+	ID int `json:"id"`
+	ImageURL string `json:"imageurl"`
+}

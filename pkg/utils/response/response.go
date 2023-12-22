f36ae80c3979af049822b700557db0e0721fa618
vinay-kumar-ps <vinaykumarps321@gmail.com>
2023-12-22 23:14:10 +0530

    added user responses

--- pkg/utils/response/response.go
+++ pkg/utils/response/response.go
@@ -1,1 +1,18 @@
 package response
+
+type Response struct {
+	StatusCode int         `json:"statuscode"`
+	Message    string      `json:"message"`
+	Data       interface{} `json:"data"`
+	Error      interface{} `json:"error"`
+}
+
+func ClientResponse(StatusCode int, message string, data interface{}, err interface{}) Response {
+	return Response{
+		StatusCode: StatusCode,
+		Message:    message,
+		Data:       data,
+		Error:      err,
+	}
+
+}

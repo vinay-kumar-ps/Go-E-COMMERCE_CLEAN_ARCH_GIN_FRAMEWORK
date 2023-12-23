dd41b59b015964cb62f1ca167e4db0c0fb39508b
vinay-kumar-ps <vinaykumarps321@gmail.com>
2023-12-23 12:09:38 +0530

    added user repositories

--- pkg/repository/interfaces/coupon.go
+++ pkg/repository/interfaces/coupon.go
@@ -1,1 +1,12 @@
 package interfaces
+
+import "ecommerce/pkg/domain"
+
+type CouponRepository interface{
+	AddCoupon (domain.Coupon)error
+	MakeCouponInvalid(id int )error
+	FindCouponDiscount(coupon string)int
+	GetCoupons (page,limit int ) ([]domain.Coupon,error)
+ValidateCoupon (coupon string)(bool,error)
+
+}

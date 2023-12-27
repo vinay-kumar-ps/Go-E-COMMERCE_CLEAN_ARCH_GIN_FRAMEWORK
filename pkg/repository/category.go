dc2e5dec375d65ac2648413686044cde3b1f40e5
vinay-kumar-ps <vinaykumarps321@gmail.com>
2023-12-27 13:36:45 +0530

    corrections

--- pkg/repository/category.go
+++ pkg/repository/category.go
@@ -12,41 +12,40 @@ import (
 type categoryRepository struct {
 	DB *gorm.DB
 }
+
 func NewCategoryRepository(db *gorm.DB) interfaces.CategoryRepository {
 	return &categoryRepository{
 		DB: db,
 	}
 }
 
-func (cat *cartRepository) AddCategory(category string) (domain.Category, error) {
+func (cat *categoryRepository) AddCategory(category string) (domain.Category, error) {
 	var b string
 	err := cat.DB.Raw("INSERT INTO categories(category)VALUES(?)RETURNING category", category).Scan(&b).Error
 	if err != nil {
-
 		return domain.Category{}, err
 	}
 	var categoryResponse domain.Category
 	err = cat.DB.Raw(`
-	   SELECT
+		SELECT 
 
-	   cat.id,
-	   cat.category
-
-	   FROM 
-	   categories cat
-	   WHERE
-	   cat.category=?
-
-	 `, b).Scan(&categoryResponse).Error
+		cat.id,
+		cat.category
+		
+		FROM
+		categories cat
+		WHERE
+		cat.category = ?
+	`, b).Scan(&categoryResponse).Error
 	if err != nil {
 		return domain.Category{}, err
 	}
 	return categoryResponse, nil
 }
+
 func (cat *categoryRepository) CheckCategory(current string) (bool, error) {
 	var response int
-	err := cat.DB.Raw("SELECT COUNT (*)FROM categories WHERE category=?", current).Scan(&response).Error
-
+	err := cat.DB.Raw("SELECT COUNT (*) FROM categories WHERE category= ?", current).Scan(&response).Error
 	if err != nil {
 		return false, err
 	}
@@ -55,19 +54,21 @@ func (cat *categoryRepository) CheckCategory(current string) (bool, error) {
 	}
 	return true, nil
 }
+
 func (cat *categoryRepository) UpdateCategory(current, new string) (domain.Category, error) {
+
 	// check database connection
+
 	if cat.DB == nil {
 		return domain.Category{}, errors.New("database connection failed while update category")
-
 	}
-	//update category
-	err := cat.DB.Exec("UPDATE categories SET category=?", new, current).Error
+
+	// update category
+	err := cat.DB.Exec("UPDATE categories SET category=? WHERE category=?", new, current).Error
 	if err != nil {
 		return domain.Category{}, err
 	}
-	//retrive updated  category
-
+	// Retrieve updated category
 	var updatedCat domain.Category
 	err = cat.DB.First(&updatedCat, "category=?", new).Error
 	if err != nil {
@@ -75,19 +76,19 @@ func (cat *categoryRepository) UpdateCategory(current, new string) (domain.Categ
 	}
 	return updatedCat, nil
 }
+
 func (cat *categoryRepository) DeleteCategory(categoryId string) error {
 	id, err := strconv.Atoi(categoryId)
 	if err != nil {
 		return errors.New("string to int conversion failed")
-
 	}
 	deleteRes := cat.DB.Exec("DELETE FROM categories WHERE id=?", id)
 	if deleteRes.RowsAffected < 1 {
 		return errors.New("no record exists with this id")
-
 	}
 	return nil
 }
+
 func (cat *categoryRepository) GetCategories(page, limit int) ([]domain.Category, error) {
 	if page == 0 {
 		page = 1
@@ -96,7 +97,6 @@ func (cat *categoryRepository) GetCategories(page, limit int) ([]domain.Category
 		limit = 10
 	}
 	offset := (page - 1) * limit
-
 	var categories []domain.Category
 	err := cat.DB.Raw("SELECT id,category FROM categories limit ? offset ?", limit, offset).Scan(&categories).Error
 	if err != nil {

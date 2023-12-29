8f9ee2627eb4a46aa72739b2f43d698dbee03bb7
vinay-kumar-ps <vinaykumarps321@gmail.com>
2023-12-29 12:19:04 +0530

    adding inventory

--- pkg/repository/inventory.go
+++ pkg/repository/inventory.go
@@ -34,7 +34,7 @@ func (ir *inventoryRepository) AddInventory(inventory models.Inventory, url stri
 	return models.InventoryResponse{}, nil
 }
 
-func (ir *inventoryRepository) UpdateImage(inventId int, url string) (models.Inventory, error) {
+func (ir *inventoryRepository) UpdatedImage(inventId int, url string) (models.Inventory, error) {
 
 	//check db connecction
 	if ir.DB == nil {
@@ -43,12 +43,61 @@ func (ir *inventoryRepository) UpdateImage(inventId int, url string) (models.Inv
 	}
 
 	//updating image
-	err := ir.DB.Exec("UPDATEN inventories SET image =? WHERE id =?",inventId).Scan(&ir.UpdateImageInventory).Error
+	err := ir.DB.Exec("UPDATEN inventories SET image =? WHERE id =?",inventId).Scan(&ir.UpdatedImageInventory).Error
 
   if err !=nil{
 	return models.Inventory{},err
   }
   //Retrive the update
-  var UpdateImageInventory models.Inventory
-  err
+  var UpdatedImageInventory models.Inventory
+  err= ir.DB.Raw("SELECT COUNT  * FROM  inventories WHERE id =?",inventId).Scan(&UpdatedImageInventory).Error
+   
+  if err!=nil{
+	return models.Inventory{},err
+
+  }
+  return UpdatedImageInventory,nil
+}
+func (ir *inventoryRepository) CheckInventory(pid int) (bool,error) {
+
+	var check int
+	err := ir.DB.Raw("SELECT COUNT (*) FROM inventories WHERE id =?",pid).Scan(&check).Error
+	 
+	if err!=nil{
+return false ,err
+	}
+	if check ==0{
+		return false,err
+	}
+	return true,nil
+}
+
+func (ir *inventoryRepository) UpdateInventory(pid int,invData models.UpdateInventory)(models.Inventory,error) {
+	if  ir.DB==nil{
+		return models.Inventory{},errors.New("databse connection failed while update inventory")
+	}
+	if invData.CategoryID !=0 {
+		if err := ir.DB.Exec("UPDATE inventories SET category_id=?,WHERE id=?",invData.CategoryID,pid).Error;err !=nil {
+
+        return models.Inventory{},err
+		}
+
+	}
+
+if invData.ProductName !="" && invData.ProductName!= "string" {
+		if err := ir.DB.Exec("UPDATE inventories SET product_name=?,WHERE id=?",invData.ProductName,pid).Error; err!=nil {
+
+        return models.Inventory{},err
+		}
+
+	}
+
+	if invData.Description !="" && invData.Description!= "string" {
+		if err := ir.DB.Exec("UPDATE inventories SET description=?,WHERE id=?",invData.Description,pid).Error; err!=nil {
+
+        return models.Inventory{},err
+		}
+
+	}
+
 }

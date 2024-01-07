b7e591550d5b383d0b9e2332f997d8ff321ff05f
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-01-07 10:26:32 +0530

    added payment

--- pkg/repository/interfaces/payment.go
+++ pkg/repository/interfaces/payment.go
@@ -4,9 +4,9 @@ import "ecommerce/pkg/domain"
 
 type PaymentRepository interface{
 	AddNewPaymentMethod(paymentMethod string)error
-	RemovePaymentMethod(paymentMethodId int )error
+	RemovePaymentMethod(paymentMethodId int)error
 	GetPaymentMethods()([]domain.PaymentMethod,error)
 	FindUsername(user_id int)(string,error)
-    FindPrice(order_id int )(float64,error)
+	FindPrice(order_id int)(float64,error)
 	UpdatePaymentDetails(orderId,paymentId,razorId string)error
 }
--- pkg/repository/payment.go
+++ pkg/repository/payment.go
@@ -7,11 +7,11 @@ import (
 	"gorm.io/gorm"
 )
 
-type PaymentRepository struct{
+type paymentRepository struct {
 	DB *gorm.DB
 }
 
-//constructor function
+// constructor function
 
 func NewPaymentRepository(DB *gorm.DB) interfaces.PaymentRepository {
 	return &paymentRepository{
@@ -19,58 +19,57 @@ func NewPaymentRepository(DB *gorm.DB) interfaces.PaymentRepository {
 	}
 }
 
-func (pr *PaymentRepository) AddNewPaymentMethod (paymentMethod string)error{
-	query := `INSERT INTO payment_method (payment_method)VALUES(?)`
-	if err:= pr.DB.Exec(query ,paymentMethod).Error; err != nil{
-		
-		return err 
+func (pr *paymentRepository) AddNewPaymentMethod(paymentMethod string) error {
+	query := `INSERT INTO payment_methods(payment_method)VALUES(?)`
+
+	if err := pr.DB.Exec(query, paymentMethod).Error; err != nil {
+		return err
 	}
 	return nil
 }
 
-func (pr *PaymentRepository) RemovePaymentMethod(paymentMethodId int ) error{
-	query := `DELETE FROM payment_methods WHERE id =?`
+func (pr *paymentRepository) RemovePaymentMethod(paymentMethodId int) error {
+	query := `DELETE FROM payment_methods WHERE id=?`
 
-	if err:= pr.DB.Exec(query ,paymentMethodId).Error; err != nil{
-		
-		return err 
+	if err := pr.DB.Exec(query, paymentMethodId).Error; err != nil {
+		return err
 	}
 	return nil
-
 }
 
-func (pr *PaymentRepository)GetPaymentMethods()([]domain.PaymentMethod,error){
+func (pr *paymentRepository) GetPaymentMethods() ([]domain.PaymentMethod, error) {
 	var paymentMethods []domain.PaymentMethod
+	err := pr.DB.Raw("SELECT * FROM payment_methods").Scan(&paymentMethods).Error
+	if err != nil {
+		return []domain.PaymentMethod{}, err
+	}
+	return paymentMethods, nil
+}
 
-	err :=pr.DB.Raw("SELECT *FROM payment_method").Scan(&paymentMethods).Error
-	if err!=nil{
-		return []domain.PaymentMethod{},err
-	}
-	return paymentMethods,nil
-}
-func (pr *PaymentRepository) FindUsername(user_id int ) (string,error){
+func (pr *paymentRepository) FindUsername(user_id int) (string, error) {
 	var userName string
-	err := pr.DB.Raw("SELECT name FROM users WHERE id=?",user_id).Scan(&userName).Error
-	if err !=nil{
-		return "",err
+	err := pr.DB.Raw("SELECT name FROM users WHERE id=?", user_id).Scan(&userName).Error
+	if err != nil {
+		return "", err
 	}
-	return userName,nil
+	return userName, nil
 }
-func (pr *PaymentRepository) FindPrice (order_id int ) (float64,error){
+
+func (pr *paymentRepository) FindPrice(order_id int) (float64, error) {
 	var price float64
 
-	err := pr.DB.Raw("SELECT price FROM orders WHERE id=?",order_id).Scan(&price).Error
-	if err !=nil{
-		return 0,err
+	err := pr.DB.Raw("SELECT price FROM orders WHERE id=?", order_id).Scan(&price).Error
+	if err != nil {
+		return 0, err
 	}
-	return price,nil
+	return price, nil
 }
 
-func (pr *PaymentRepository) updatePaymentDetails (orderId ,paymentId ,razorId string)error{
-	status :="paid"
-	 query := `UPDATE orders SET payment_status =$1,payment_id=$3 WHERE id $2`
-	 if err := pr.DB.Exec(query ,status,orderId,paymentId).Error;err !=nil{
+func (pr *paymentRepository) UpdatePaymentDetails(orderId, paymentId, razorId string) error {
+	status := "PAID"
+	query := `UPDATE orders SET payment_status=$1,payment_id=$3 WHERE id=$2`
+	if err := pr.DB.Exec(query, status, orderId, paymentId).Error; err != nil {
 		return err
-	 }
-	 return nil
+	}
+	return nil
 }

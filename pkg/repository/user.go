40aff0a5b576f3fc7ead59c14efc0aab3bc586c1
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-01-09 22:47:08 +0530

    adding user

--- pkg/repository/user.go
+++ pkg/repository/user.go
@@ -1,7 +1,22 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"ecommerce/pkg/repository/interfaces"
 
-type userRepository struct{
-	DB *gorm.DB
+	"gorm.io/gorm"
+)
+
+func NewUserRepository(DB *gorm.DB) interfaces.UserRepository{
+	return &userRepository[
+		DB: DB,
+	]
+
+}
+ func (ur *userRepository) CheckUserAvailability(email string )bool{
+
+var userCount int
+
+err := ur.DB.Raw("SELECT COUNT (*) FROM users WHERE email=? ",email).Scan(&userCount).Error
+
+	
 }

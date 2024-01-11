5382c3b4204e643a2b8bd7bbb3dd69660cd27e01
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-01-11 23:08:43 +0530

    adding wallet

--- pkg/repository/wallet.go
+++ pkg/repository/wallet.go
@@ -1,1 +1,17 @@
 package repository
+
+import (
+	"ecommerce/pkg/repository/interfaces"
+
+	"gorm.io/gorm"
+)
+
+type walletRepository struct {
+	DB *gorm.DB
+}
+// 
+func NewWalletRepository ( DB *gorm.DB) interfaces.WalletRepository{
+	return &walletRepository{
+		DB: DB,
+	}
+}

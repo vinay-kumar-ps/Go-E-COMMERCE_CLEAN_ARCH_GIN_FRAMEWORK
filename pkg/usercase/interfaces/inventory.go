ae1644d7fec1fa815d240f3ca195541606b60e79
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-01-16 23:07:04 +0530

    added inventory in usecase

--- pkg/usercase/interfaces/inventory.go
+++ pkg/usercase/interfaces/inventory.go
@@ -1,1 +1,20 @@
-package interface
+package interfaces
+
+import (
+	"ecommerce/pkg/utils/models"
+	"mime/multipart"
+)
+
+type InventoryUsecase interface{
+	AddInventory (inventory models.Inventory,image *multipart.FileHeader)(models.InventoryResponse,error)
+	UpdateInventory (invID int, invDate models.UpdateInventory)(models.Inventory,error)
+	UpdateImage(invID int ,image *multipart.FileHeader)(models.Inventory,error)
+	DeleteInventory (id string)error
+
+	ShowIndividualProducts(id string) (models.InventoryDetails,error)
+	ListProduts(page int, limit int)([]models.InventoryList,error)
+	SearchProducts(key string,page ,limit int)([]models.InventoryList,error)
+	GetCategoryProducts(cartID int ,page,limit int )([]models.InventoryList,error)
+	AddImage(product_id int ,image *multipart.FileHeader)(models.InventoryResponse,error)
+	DeleteImage(product_id,image_id int)error
+}

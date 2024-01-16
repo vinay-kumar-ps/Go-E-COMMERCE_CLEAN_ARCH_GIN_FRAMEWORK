5f65d495d34b227256b000a1e6e48140f3eaccc9
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-01-16 22:37:33 +0530

    added usecase interface admin

--- pkg/usercase/interfaces/admin.go
+++ pkg/usercase/interfaces/admin.go
@@ -3,7 +3,7 @@ package interfaces
 import "ecommerce/pkg/utils/models"
 
 type AdminUsecase interface{
-	LoginHandler(adminDetails models.AdminLogin) (models.AdminToken error)
+	LoginHandler(adminDetails models.AdminLogin) (models.AdminToken, error)
 	BlockUser(id string)error
 	UnblockUser(id string)error
 	GetUsers(page ,limit int) ([]models.UserDetailsAtAdmin,error)

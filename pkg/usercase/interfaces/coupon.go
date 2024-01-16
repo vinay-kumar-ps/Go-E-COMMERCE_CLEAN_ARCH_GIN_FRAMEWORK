aca7777b29116f012e3df121497fb654f81c1b27
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-01-16 22:49:01 +0530

    added usecase coupon

--- pkg/usercase/interfaces/coupon.go
+++ pkg/usercase/interfaces/coupon.go
@@ -1,1 +1,9 @@
-package interface
+package interfaces
+
+import "ecommerce/pkg/domain"
+
+type CouponUsecase interface{
+	AddCoupon (coupon domain.Coupon)error
+	MakeCouponInvalid(id int)error
+	GetCoupons(page,limit int)([]domain.Coupon,error)
+}

ae2910873620df1e66f77555d74370d99110dd42
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-01-16 22:41:34 +0530

    added usecase interface category

--- pkg/usercase/interfaces/category.go
+++ pkg/usercase/interfaces/category.go
@@ -1,1 +1,11 @@
-package interface
+package interfaces
+
+import "ecommerce/pkg/domain"
+
+type CategoryUsecase interface{
+	AddCategory (category string) (domain.Category,error)
+	UpdateCategory (current ,new string)(domain.Category,error)
+	DeleteCategory(CategoryId string)error
+	GetCategories(page ,limit int) ([]domain.Category,error)
+	
+}

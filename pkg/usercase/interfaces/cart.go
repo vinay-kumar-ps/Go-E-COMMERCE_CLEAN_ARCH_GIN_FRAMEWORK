2669b97bd9348e1e09a6350f7faf808b9dbec80b
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-01-16 22:35:26 +0530

    added usecase interface cart

--- pkg/usercase/interfaces/cart.go
+++ pkg/usercase/interfaces/cart.go
@@ -1,1 +1,8 @@
-package interface
+package interfaces
+
+import "ecommerce/pkg/utils/models"
+
+type CartUsecase interface{
+	AddToCart (user_id ,inventory_id int)error
+	CheckOut(id int)(models.CheckOut,error)
+}

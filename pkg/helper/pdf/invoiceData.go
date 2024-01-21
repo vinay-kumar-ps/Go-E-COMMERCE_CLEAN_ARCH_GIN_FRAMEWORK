57a750e664a9d42d8330c750f3b78347c0630be1
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-01-21 11:29:26 +0530

    added invoice in helper file

--- pkg/helper/pdf/invoiceData.go
+++ pkg/helper/pdf/invoiceData.go
@@ -1,5 +1,7 @@
 package pdf
 
+import "errors"
+
 type InvoiceData struct{
 	Title  string
 	Quantity int 
@@ -19,4 +21,25 @@ func (d *InvoiceData) ReturnItemTotalAmount()float64{
 func (d *InvoiceData)ReturnItemPrice()float64{
 	returnPrice :=float64(d.price)/100
 	return returnPrice
+}
+func NewInvoiceData(title string,qty int ,price interface{})(*InvoiceData,error){
+	var convertedPrice int
+	switch priceValue :=price.(type){
+
+	case int :
+		convertedPrice = priceValue *100
+	case float32:
+		convertedPrice = int(priceValue)*100
+	case float64:
+		convertedPrice = int(priceValue)*100
+	default:
+		return nil, errors.New("type not permitted")
+
+	}
+	return &InvoiceData{
+		Title: title,
+		Quantity: qty,
+		price: convertedPrice,
+	},nil
+
 }

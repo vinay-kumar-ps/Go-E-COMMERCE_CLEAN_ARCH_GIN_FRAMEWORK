e3f48410de37c8ada07f87845692863ac5f035fa
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-01-24 22:40:08 +0530

    fixing errors

--- pkg/usecase/inventory.go
+++ pkg/usecase/inventory.go
@@ -1,2 +1,19 @@
 package usecase
 
+import (
+	"ecommerce/pkg/repository/interfaces"
+	services "ecommerce/pkg/usecase/interfaces"
+)
+
+type InventoryUsecase struct {
+	invRepo interfaces.InventoryRespository
+}
+
+//contructor function
+
+func NewInventoryUsecase(invRepo interfaces.InventoryRespository) services.InventoryUsecase {
+	return InventoryUsecase{
+		invRepo: invRepo,
+	}
+}
+func 

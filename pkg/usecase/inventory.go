b3616e0aa95bc723f9b7c70f6c31c6516909d7af
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-01-28 22:53:45 +0530

    fixing

--- pkg/usecase/inventory.go
+++ pkg/usecase/inventory.go
@@ -1,8 +1,11 @@
 package usecase
 
 import (
+	"ecommerce/pkg/helper"
 	"ecommerce/pkg/repository/interfaces"
 	services "ecommerce/pkg/usecase/interfaces"
+	"ecommerce/pkg/utils/models"
+	"mime/multipart"
 )
 
 type InventoryUsecase struct {
@@ -16,4 +19,27 @@ func NewInventoryUsecase(invRepo interfaces.InventoryRespository) services.Inven
 		invRepo: invRepo,
 	}
 }
-func 
+func (invU *InventoryUsecase)AddInventory(inventory models.Inventory,image *multipart.FileHeader)(models.InventoryResponse,error){
+	url ,err :=helper.AddImageToS3(image)
+ 
+	if err !=nil{
+		return models.InventoryResponse{},err
+	}
+	inventory.Image =url
+	//send the url save in db
+	inventoryResponse,err :=invU.AddInventory(inventory,url)
+	if err!=nil{
+		return models.InventoryResponse{},err
+
+	}
+	return inventoryResponse,nil
+}
+func (invU *InventoryUsecase)UpdateImage(invID int ,image *multipart.FileHeader)(models.Inventory,error){
+
+	url,err :=helper.AddImageToS3(image)
+	if err !=nil{
+		return models.Inventory{},err
+	}
+	inve
+}
+

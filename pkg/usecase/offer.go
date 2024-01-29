f79b25046f4e10d45c5e3804c5aef2bb02f875a4
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-01-29 22:42:54 +0530

    addded offer in usecase

--- pkg/usecase/offer.go
+++ pkg/usecase/offer.go
@@ -1,2 +1,42 @@
 package usecase
 
+import (
+	"ecommerce/pkg/domain"
+	interfaces "ecommerce/pkg/repository/interfaces"
+	services "ecommerce/pkg/usecase/interfaces"
+	"ecommerce/pkg/utils/models"
+	"errors"
+)
+
+type offerUsecse struct{
+	offerRepo interfaces.OfferRepository
+
+
+}
+//constructor function
+
+func NewOfferUsecase(offerRepo interfaces.OfferRepository) services.OfferUsecase{
+	return &offerUsecse{
+		offerRepo:offerRepo,
+	}
+}
+func (offU *offerUsecse)AddNewOffer(model models.CreateOffer)error{
+	if err := offU.offerRepo.AddNewOffer(model);err!=nil{
+		return errors.New("adding offer failed")
+	}
+	return nil
+}
+func(offU *offerUsecse)MakeOfferExpire(catID int )error{
+	if err :=offU.offerRepo.MakeOfferExpired(catID);err!=nil{
+		return err
+	}
+	return nil
+}
+
+func (offU *offerUsecse) GetOffers(page, limit int) ([]domain.Offer, error) {
+	offers, err := offU.offerRepo.GetOffers(page, limit)
+	if err != nil {
+		return []domain.Offer{}, err
+	}
+	return offers, nil
+}

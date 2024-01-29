4999ce44355a973174467d9709c14bc7f1e94b9a
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-01-29 23:11:21 +0530

    fixing

--- pkg/usecase/order.go
+++ pkg/usecase/order.go
@@ -1,2 +1,16 @@
 package usecase
 
+import (
+	interfaces "ecommerce/pkg/repository/interfaces"
+    services"ecommerce/pkg/usecase/interfaces"  
+)
+
+type OrderUsecase struct{
+	orderRepo  interfaces.OrderRepository
+	userUsecase  services.UserUsecase
+	walletRepo interfaces.WalletRepository
+	couponRepo  interfaces.CouponRepository
+
+}
+
+
--- pkg/usecase/payment.go
+++ pkg/usecase/payment.go
@@ -1,1 +1,11 @@
 package usecase
+
+import "ecommerce/pkg/repository/interfaces"
+
+type paymentUsecase struct{
+	paymentRepo interfaces.PaymentRepository
+	userRepo interfaces.UserRepository
+}
+
+//constructor function
+func New()

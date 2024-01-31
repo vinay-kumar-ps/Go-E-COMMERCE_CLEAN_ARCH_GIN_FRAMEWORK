74e12455f72a85a56a9dfce7c8381305e89199ae
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-01-31 23:31:14 +0530

    added wishlist in usecase

--- pkg/usecase/wishlist.go
+++ pkg/usecase/wishlist.go
@@ -1,1 +1,103 @@
 package usecase
+
+import (
+	interfaces "ecommerce/pkg/repository/interfaces"
+	services "ecommerce/pkg/usecase/interfaces"
+	"ecommerce/pkg/utils/models"
+	"errors"
+)
+type wishlistUsecase struct{
+	wishRepo interfaces.WishlistRepository
+}
+//constructor function
+func NewWishlistUsecase (wishRepo interfaces.WishlistRepository)services.WishlistUsecase{
+	return &wishlistUsecase{
+		wishRepo: wishRepo,
+	}
+}
+
+
+func (wlU *wishlistUsecase) GetWishlistID(userID int )(int, error){
+	wishlistId,err :=wlU.wishRepo.GetWishlistId(userID)
+	if err !=nil{
+		return 0,err
+	}
+	return wishlistId,nil
+}
+
+func (wlU *wishlistUsecase) GetWishlist(id int )([]models.GetWishlist,error){
+//find wishlist id
+wishlistId ,err :=wlU.wishRepo.GetWishlistId(id)
+if err !=nil{
+	return []models.GetWishlist{},errors.New("couldn't find products inside wishlist")
+}
+//find products inside wishlist
+products,err :=wlU.wishRepo.GetProductsInWishlist(wishlistId)
+//find product name
+var productName []string
+
+for i :=range products {
+	prdName ,err :=wlU.wishRepo.FindProductNames(products[i])
+	if err !=nil{
+		return []models.GetWishlist{},err	
+	}
+	productName=append(productName, prdName)
+}
+//find price 
+var productPrice []float64
+for i := range products {
+	p ,err :=wlU.wishRepo.FindPrice(products[i])
+	if err !=nil{
+		return []models.GetWishlist{},err
+	}
+	productPrice =append(productPrice, p)
+}
+//find category
+var category []string
+
+for i:=range products {
+	c ,err :=wlU.wishRepo.FindCategory(products[i])
+	if err !=nil{
+	return []models.GetWishlist{},errors.New("couldn't find category")
+	}
+	category =append(category, c)
+}
+var getWishlist[]models.GetWishlist
+
+for i:= range products {
+	var get models.GetWishlist
+	get.ProductName =productName[i]
+	get .Price =productPrice[i]
+	get.Category=category[i]
+	getWishlist =append(getWishlist, get)
+}
+return getWishlist,nil
+}
+
+func(wlU *wishlistUsecase)AddToWishlist(user_id ,inventory_id int)error {
+	//find user wishlist id
+	wishlistId,err :=wlU.wishRepo.GetWishlistId(user_id)
+	if err !=nil{
+		return errors.New("couldn't find wishlist id")
+
+	}
+	//if user hasa no cart ,create new cart
+	if wishlistId==0{
+		wishlistId,err :=wlU.wishRepo.CreateNewWishlist(user_id)
+		if err !=nil{
+			return err
+		}
+		//add products to line items
+		if err :=wlU.wishRepo.AddWishlistItem(wishlistId,inventory_id);err!=nil{
+			return errors.New("add to wishlist failed ")
+		}
+	}
+	return nil
+}
+func (wlU *wishlistUsecase) RemoveFromWishlist(id int, inventoryID int) error {
+	err := wlU.wishRepo.RemoveFromWishlist(id, inventoryID)
+	if err != nil {
+		return err
+	}
+	return nil
+}

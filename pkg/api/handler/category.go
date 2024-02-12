080be11b590a19f32913443578f2373c9c177f4e
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-02-12 22:59:39 +0530

    added category functuon in handler

--- pkg/api/handler/category.go
+++ pkg/api/handler/category.go
@@ -1,7 +1,10 @@
 package handler
 
 import (
+	"ecommerce/pkg/domain"
 	services "ecommerce/pkg/usecase"
+
+	"github.com/gin-gonic/gin"
 )
 
 type CategoryHandler struct {
@@ -13,4 +16,18 @@ func NewCategoryHandler(usecase services.CategoryUseCase) *CategoryHandler {
 		CategoryUseCase: usecase,
 	}
 }
+// @Summary		Add Category
+// @Description	Admin can add new categories for products
+// @Tags			Admin
+// @Accept			json
+// @Produce		    json
+// @Param			category	body	domain.Category	true	"category"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/admin/category [post]
 
+func(cat * CategoryHandler) AddCategory(c *gin.Context){
+	var category domain.Category
+	if err
+}

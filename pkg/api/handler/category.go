c3cec686ae50a89be30a1f69677601d3a9e81760
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-02-12 22:57:44 +0530

    added category functuon in handler

--- pkg/api/handler/category.go
+++ pkg/api/handler/category.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	services "ecommerce/pkg/usecase"
+)
+
+type CategoryHandler struct {
+	CategoryUseCase services.CategoryUseCase
+
+}
+func NewCategoryHandler(usecase services.CategoryUseCase) *CategoryHandler {
+	return &CategoryHandler{
+		CategoryUseCase: usecase,
+	}
+}
+

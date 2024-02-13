65125ba212bf15abe48fd99b5319b2de36862caf
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-02-13 13:08:54 +0530

    added category functuon in handler

--- pkg/api/handler/category.go
+++ pkg/api/handler/category.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"ecommerce/pkg/domain"
 	services "ecommerce/pkg/usecase"
+	"ecommerce/pkg/utils/models"
 	"ecommerce/pkg/utils/response"
 	"net/http"
 
@@ -29,14 +30,14 @@ func NewCategoryHandler(usecase services.CategoryUseCase) *CategoryHandler {
 // @Failure		500	{object}	response.Response{}
 // @Router			/admin/category [post]
 
-func(cat * CategoryHandler) AddCategory(c *gin.Context){
+func(Cat * CategoryHandler) AddCategory(c *gin.Context){
 	var category domain.Category
 	if err := c.BindJSON(&category);err !=nil{
 		errorRes :=response.ClientResponse(http.StatusBadRequest,"fields provided are in wrong format",nil,err.Error())
 		c.JSON(http.StatusBadRequest,errorRes)
 		return
 	}
-	categoryResponse ,err:= cat.CategoryUseCase.AddCategory(category)
+	categoryResponse ,err:= Cat.CategoryUseCase.AddCategory(category)
 	if err !=nil{
 		errorRes := response.ClientResponse(http.StatusBadRequest,"could not add the category",nil,err.Error())
 		c.JSON(http.StatusBadRequest,errorRes)
@@ -46,3 +47,40 @@ func(cat * CategoryHandler) AddCategory(c *gin.Context){
 	c.JSON(http.StatusOK,successRes)
 
 }
+// @Summary		Update Category
+// @Description	Admin can update name of a category into new name
+// @Tags			Admin
+// @Accept			json
+// @Produce		    json
+// @Param			set_new_name	body	models.SetNewName	true	"set new name"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/admin/category [put]
+
+func (Cat *CategoryHandler) UpdateCategory(c *gin.Context){
+	var p models.SetNewName
+	if err :=c.BindJSON(&p);err !=nil{
+		erroRes:=response.ClientResponse(http.StatusBadRequest,"fields provided in wrong format",nil,err.Error())
+		c.JSON(http.StatusBadRequest,erroRes)
+		return
+	}
+	a,err :=Cat.CategoryUseCase.UpdateCategory(p.Current,p.New)
+	if err !=nil{
+		errorRes :=response.ClientResponse(http.StatusBadRequest,"could not update the category ",nil,err.Error())
+	    c.JSON(http.StatusBadRequest,errorRes)
+		return
+	}
+	succesRes :=response.ClientResponse(http.StatusOK,"successfulyy renamed the category",a,ni)
+	c.JSON(http.StatusOK,succesRes)
+}
+// @Summary		Delete Category
+// @Description	Admin can delete a category
+// @Tags			Admin
+// @Accept			json
+// @Produce		    json
+// @Param			id	query	string	true	"id"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/admin/category [delete]

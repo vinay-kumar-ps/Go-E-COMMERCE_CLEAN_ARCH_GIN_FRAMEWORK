e446c1b55e8b595bb51ca26c1554479a2d3d6f0c
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-02-14 22:02:24 +0530

    added category in handler

--- pkg/api/handler/coupon.go
+++ pkg/api/handler/coupon.go
@@ -1,1 +1,15 @@
 package handler
+
+import(
+	services "ecommerce/pkg/usecase/interfaces"
+	)
+
+type CouponHandler struct {
+	uscase services.CouponUsecase
+}
+func NewCouponHandler(use services.CouponUsecase) *CouponHandler {
+	return &CouponHandler{
+		uscase: use,
+	}
+}
+

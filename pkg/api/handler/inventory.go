08daef0a2ee46d8fd2921203634d891d866d60a5
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-02-16 23:00:30 +0530

    added inventory in handler

--- pkg/api/handler/inventory.go
+++ pkg/api/handler/inventory.go
@@ -1,1 +1,14 @@
 package handler
+
+import(
+	services "ecommerce/pkg/usecase/interfaces"
+)
+
+type InventoryHandler struct{
+	InventoryUseCase services.InventoryUseCase
+}
+func NewInventoryHandler (usecase services.InventoryUseCase)*InventoryHandler{
+	return &InventoryHandler{
+		InventoryUseCase: usecase,
+	}
+}

6b04a223f0ec735469dbe9550cb17d74a26c212c
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-02-17 22:37:04 +0530

    added inventory in handler

--- pkg/api/handler/inventory.go
+++ pkg/api/handler/inventory.go
@@ -1,14 +1,88 @@
 package handler
 
-import(
+import (
 	services "ecommerce/pkg/usecase/interfaces"
+	"ecommerce/pkg/utils/models"
+	"ecommerce/pkg/utils/response"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
-type InventoryHandler struct{
+type InventoryHandler struct {
 	InventoryUseCase services.InventoryUseCase
 }
-func NewInventoryHandler (usecase services.InventoryUseCase)*InventoryHandler{
+
+func NewInventoryHandler(usecase services.InventoryUseCase) *InventoryHandler {
 	return &InventoryHandler{
 		InventoryUseCase: usecase,
 	}
 }
+
+// @Summary		Add Inventory
+// @Description	Admin can add new  products
+// @Tags			Admin
+// @Accept			multipart/form-data
+// @Produce		    json
+// @Param			category_id		formData	string	true	"category_id"
+// @Param			product_name	formData	string	true	"product_name"
+// @Param			size		formData	string	true	"size"
+// @Param			price	formData	string	true	"price"
+// @Param			stock		formData	string	true	"stock"
+// @Param           image      formData     file   true   "image"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/admin/inventories [post]
+func (i *InventoryHandler) AddInventory(c *gin.Context){
+
+	var inventory models.AddInventories
+	categoryID,err :=strconv.Atoi(c.Request.FormValue("category_id"))
+	if err !=nil{
+		erroRes :=response.ClientResponse(http.StatusBadRequest,"form file error",nil,err.Error())
+		c.JSON(http.StatusBadRequest,erroRes)
+		return
+	}
+	productName := c.Request.FormValue("product_name")
+	size := c.Request.FormValue("size")
+	p, err := strconv.Atoi(c.Request.FormValue("price"))
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "form file error", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+	price := float64(p)
+	stock, err := strconv.Atoi(c.Request.FormValue("stock"))
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "form file error", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	inventory.CategoryID = categoryID
+	inventory.ProductName = productName
+	inventory.Size = size
+	inventory.Price = price
+	inventory.Stock = stock
+
+	file, err := c.FormFile("image")
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "retrieving image from form error", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	InventoryResponse, err := i.InventoryUseCase.AddInventory(inventory, file)
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "Could not add the Inventory", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	successRes := response.ClientResponse(http.StatusOK, "Successfully added Inventory", InventoryResponse, nil)
+	c.JSON(http.StatusOK, successRes)
+
+
+}
+

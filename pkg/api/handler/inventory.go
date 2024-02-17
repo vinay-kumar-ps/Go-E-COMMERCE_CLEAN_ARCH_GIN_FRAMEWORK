764bcb021e2e3607bda9e1a96fbad1a917bea6dd
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-02-17 22:46:13 +0530

    added inventory in handler

--- pkg/api/handler/inventory.go
+++ pkg/api/handler/inventory.go
@@ -86,3 +86,232 @@ func (i *InventoryHandler) AddInventory(c *gin.Context){
 
 }
 
+// @Summary		Update Stock
+// @Description	Admin can update stock of the inventories
+// @Tags			Admin
+// @Accept			json
+// @Produce		    json
+// @Param			add-stock	body	models.InventoryUpdate	true	"update stock"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/admin/inventories [put]
+
+func (i *InventoryHandler) UpdateInventory(c *gin.Context) {
+
+	var p models.InventoryUpdate
+
+	if err := c.BindJSON(&p); err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "fields provided are in wrong format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	a, err := i.InventoryUseCase.UpdateInventory(p.Productid, p.Stock)
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "could not update the inventory stock", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	successRes := response.ClientResponse(http.StatusOK, "Successfully updated the inventory stock", a, nil)
+	c.JSON(http.StatusOK, successRes)
+
+}
+
+// @Summary		Delete Inventory
+// @Description	Admin can delete a product
+// @Tags			Admin
+// @Accept			json
+// @Produce		    json
+// @Param			id	query	string	true	"id"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/admin/inventories [delete]
+func (i *InventoryHandler) DeleteInventory(c *gin.Context) {
+
+	inventoryID := c.Query("id")
+	err := i.InventoryUseCase.DeleteInventory(inventoryID)
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "fields provided are in wrong format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	successRes := response.ClientResponse(http.StatusOK, "Successfully deleted the inventory", nil, nil)
+	c.JSON(http.StatusOK, successRes)
+
+}
+
+// @Summary		Show Product Details
+// @Description	user can view the details of the product
+// @Tags			User
+// @Accept			json
+// @Produce		    json
+// @Param			id	query	string	true	"id"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/users/home/products/details [get]
+func (i *InventoryHandler) ShowIndividualProducts(c *gin.Context) {
+
+	id := c.Query("id")
+	product, err := i.InventoryUseCase.ShowIndividualProducts(id)
+
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "path variables in wrong format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	successRes := response.ClientResponse(http.StatusOK, "Product details retrieved successfully", product, nil)
+	c.JSON(http.StatusOK, successRes)
+
+}
+
+// @Summary		List Products
+// @Description	user can view the list of available products
+// @Tags			User
+// @Accept			json
+// @Produce		    json
+// @Param			page	query  string 	true	"page"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/users/home/products [get]
+func (i *InventoryHandler) ListProductsForUser(c *gin.Context) {
+	pageStr := c.Query("page")
+	page, err := strconv.Atoi(pageStr)
+
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "page number not in right format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	id := c.MustGet("id")
+
+	userID, ok := id.(int)
+	if !ok {
+		errorRes := response.ClientResponse(http.StatusForbidden, "problem in identifying user from the context", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	products, err := i.InventoryUseCase.ListProductsForUser(page, userID)
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "could not retrieve records", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+	successRes := response.ClientResponse(http.StatusOK, "Successfully got all records", products, nil)
+	c.JSON(http.StatusOK, successRes)
+}
+
+// @Summary		Search Products
+// @Description	user can search with a key and get the list of  products similar to that key
+// @Tags			User
+// @Accept			json
+// @Produce		    json
+// @Param			key	body	models.Search	true	"search"
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/users/search [post]
+func (i *InventoryHandler) SearchProducts(c *gin.Context) {
+
+	var searchkey models.Search
+	if err := c.BindJSON(&searchkey); err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "fields provided are in wrong format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	results, err := i.InventoryUseCase.SearchProducts(searchkey.Key)
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "could not retrieve the records", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	successRes := response.ClientResponse(http.StatusOK, "Successfully got all records", results, nil)
+	c.JSON(http.StatusOK, successRes)
+}
+
+func (i *InventoryHandler) UpdateProductImage(c *gin.Context) {
+
+	id, err := strconv.Atoi(c.Query("id"))
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "parameter problem", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	file, err := c.FormFile("image")
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "retrieving image from form error", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	err = i.InventoryUseCase.UpdateProductImage(id, file)
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "Could not change the image", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	successRes := response.ClientResponse(http.StatusOK, "Successfully changed image", nil, nil)
+	c.JSON(http.StatusOK, successRes)
+
+}
+
+func (i *InventoryHandler) EditInventoryDetails(c *gin.Context) {
+
+	id, err := strconv.Atoi(c.Query("id"))
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "parameter problem", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	var model models.EditInventoryDetails
+
+	err = c.BindJSON(&model)
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "parameter problem", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	err = i.InventoryUseCase.EditInventoryDetails(id, model)
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "Could not edit the details", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	successRes := response.ClientResponse(http.StatusOK, "Successfully edited details", nil, nil)
+	c.JSON(http.StatusOK, successRes)
+
+}
+
+func (i *InventoryHandler) ListProductsForAdmin(c *gin.Context) {
+	pageStr := c.Query("page")
+	page, err := strconv.Atoi(pageStr)
+
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "page number not in right format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	products, err := i.InventoryUseCase.ListProductsForAdmin(page)
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "could not retrieve records", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+	successRes := response.ClientResponse(http.StatusOK, "Successfully got all records", products, nil)
+	c.JSON(http.StatusOK, successRes)
+}

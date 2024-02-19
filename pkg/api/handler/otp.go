c960d327852b23280942674ebeda7b7b1194e49f
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-02-19 22:54:59 +0530

    added order in handler

--- pkg/api/handler/otp.go
+++ pkg/api/handler/otp.go
@@ -1,1 +1,78 @@
 package handler
+
+import (
+	services "ecommerce/pkg/usecase/interfaces"
+	models "ecommerce/pkg/utils/models"
+	response "ecommerce/pkg/utils/response"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type OtpHandler struct {
+	otpUseCase services.OtpUseCase
+}
+
+func NewOtpHandler(useCase services.OtpUseCase) *OtpHandler {
+	return &OtpHandler{
+		otpUseCase: useCase,
+	}
+}
+
+// @Summary		Send OTP
+// @Description	OTP login send otp
+// @Tags			User
+// @Accept			json
+// @Produce		    json
+// @Param			otp  body  models.OTPData true	"otp-data"
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/users/otplogin [post]
+func (ot *OtpHandler) SendOTP(c *gin.Context) {
+
+	var phone models.OTPData
+	if err := c.BindJSON(&phone); err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "fields provided are in wrong format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+	}
+
+	err := ot.otpUseCase.SendOTP(phone.PhoneNumber)
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "Could not send OTP", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+	successRes := response.ClientResponse(http.StatusOK, "OTP sent successfully", nil, nil)
+	c.JSON(http.StatusOK, successRes)
+
+}
+
+// @Summary		Verify OTP
+// @Description	OTP login verify otp
+// @Tags			User
+// @Accept			json
+// @Produce		    json
+// @Param			otp  body  models.VerifyData  true	"otp-verify"
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/users/verifyotp [post]
+func (ot *OtpHandler) VerifyOTP(c *gin.Context) {
+
+	var code models.VerifyData
+	if err := c.BindJSON(&code); err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "fields provided are in wrong format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	users, err := ot.otpUseCase.VerifyOTP(code)
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "Could not verify OTP", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	successRes := response.ClientResponse(http.StatusOK, "Successfully verified OTP", users, nil)
+	c.JSON(http.StatusOK, successRes)
+
+}

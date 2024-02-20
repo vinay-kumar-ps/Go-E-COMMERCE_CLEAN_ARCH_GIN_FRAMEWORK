6959ddeeb4fb4a30da1774852a7ad6de316b49e4
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-02-20 23:18:27 +0530

    added router

--- pkg/routes/adminRoutes.go
+++ pkg/routes/adminRoutes.go
@@ -33,6 +33,54 @@ engine.Use(middleware.AdminAuthMiddleware)
 		usermanagement.PUT("/unblock",adminHandler.UnBlockUser)
 	}
 	categorymanagement :=engine.Group("/category")
+	{
+		categorymanagement.GET("",categoryHandler.GetCategory)
+		categorymanagement.POST("",categoryHandler.AddCategory)
+		categorymanagement.PUT("",categoryHandler.UpdateCategory)
+		categorymanagement.DELETE("",categoryHandler.DeleteCategory)
+	}
+	inventorymanagement := engine.Group("/inventories")
+	{
+		inventorymanagement.GET("",inventoryHandler.ListProductsForAdmin)
+		inventorymanagement.POST("",inventoryHandler.AddInventory)
+		inventorymanagement.DELETE("",inventoryHandler.DeleteInventory)
+		inventorymanagement.PUT("/details",inventoryHandler.EditInventoryDetails)
+
+		inventorymanagement.PUT("/:id/stock",inventoryHandler.UpdateInventory)
+		inventorymanagement.PUT("/:id/image",inventoryHandler.UpdateProductImage)
+		// inventorymanagement.PUT("/id/details",inventoryHandler.UpdateProductDetails)
+	}
+
+	payment := engine.Group("/payment-method")
+	{
+		payment.POST("", adminHandler.NewPaymentMethod)
+		payment.GET("", adminHandler.ListPaymentMethods)
+		payment.DELETE("", adminHandler.DeletePaymentMethod)
+	}
+
+	orders := engine.Group("/orders")
+	{
+		orders.PUT("/status", orderHandler.EditOrderStatus)
+		orders.PUT("/payment-status", orderHandler.MakePaymentStatusAsPaid)
+		orders.GET("", orderHandler.AdminOrders)
+		orders.GET("/:id", orderHandler.GetIndividualOrderDetails)
+	}
+
+	coupons := engine.Group("/coupons")
+	{
+		coupons.GET("", couponHandler.GetAllCoupons)
+		coupons.POST("", couponHandler.CreateNewCoupon)
+		coupons.DELETE("", couponHandler.MakeCOuponInvalid)
+		//reactivation of coupons
+		coupons.PUT("", couponHandler.ReActivateCoupon)
+	}
+
+	offers := engine.Group("/offers")
+	{
+		offers.GET("", offerHandler.GetOffers)
+		offers.POST("", offerHandler.AddNewOffer)
+		offers.DELETE("", offerHandler.MakeOfferExpire)
+	}
 }
 
 }

66010140e766369be95c06624f9293fbfbdf79e8
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-02-21 14:06:46 +0530

    adding routes. user_routes

--- pkg/routes/userRoutes.go
+++ pkg/routes/userRoutes.go
@@ -1,1 +1,22 @@
 package routes
+
+import (
+	"ecommerce/pkg/api/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(engine *gin.RouterGroup,
+userHandler *handler.UserHandler,
+otpHandler *handler.OtpHandler,
+inventoryHandler *handler.InventoryHandler
+orderHandler *handler.OrderHandler
+carthandler *handler.CartHandler
+paymenthandler *handler.PaymentHandler
+wishlistHandler *handler.WishlistHandler
+categoryHandler *handler.CategoryHandler
+couponHandler *handler.CouponHandler
+
+
+
+)
